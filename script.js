8d16146f15a01917aa8d52f33071cdc7c5458585
Cyber Quils <105954716+Aquilavibes@users.noreply.github.com>
2023-09-10 19:59:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let utc = document.getElementById('timing');
 const today = new Date() 
 setInterval(() {
-        updateTime(utc);
+        updateTime(utc++);
     }, 1000);
 utc.innerHTML = today.getTime();
         
