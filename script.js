3cbee05ce7f98668750d164fff3a5d022d28ece0
Cyber Quils <105954716+Aquilavibes@users.noreply.github.com>
2023-09-10 19:40:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
         
 let utc = document.querySelector('[data-testid="currentDateAndTime"]');
 const today = new Date() 
-setInterval( function () {
+setInterval(() {
         updateTime(utc);
     }, 1000);
 utc.innerHTML = today.getTime();
