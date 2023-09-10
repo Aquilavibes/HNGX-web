847dcdccbdc8a1ea8af50bc8076d8f62fb3e105a
Cyber Quils <105954716+Aquilavibes@users.noreply.github.com>
2023-09-10 20:57:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const currentDateElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    const currentTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
 
-let today = new Date()        
-let utc = document.getElementById('timing');
-today.innerHTML = getTime();    
+
+    // Set the data values
+    currentDateElement.textContent = getCurrentDate();
+    updateTime(currentTimeElement);
+
+    // Update current time every second
+    setInterval(function () {
+        updateTime(currentTimeElement);
+    }, 1000);
+});
+
+function getCurrentDate() {
+    const currentDate = new Date();
+    const options = { weekday: 'long'};
+    return currentDate.toLocaleDateString(undefined, options);
+}
+
+function updateTime(element) {
+    const currentTime = Date.now();
+    element.textContent = currentTime;
+}
+  
